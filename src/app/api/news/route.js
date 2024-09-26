706200ca8b38864ddac779e5716683203daeb9c2
Eavilesz <ernesto-av@hotmail.com>
2024-09-25 21:18:49 -0500

    first draft

--- src/app/api/news/route.js
+++ src/app/api/news/route.js
@@ -2,11 +2,27 @@ import { connectDB } from '@/app/libs/mongodb';
 import News from '@/app/models/news';
 import { NextResponse } from 'next/server';
 
-export async function GET() {
+export async function GET(request) {
   await connectDB();
 
-  const news = await News.find();
-  return NextResponse.json(news);
+  const { searchParams } = new URL(request.url);
+  const page = parseInt(searchParams.get('page') || '1');
+  const limit = parseInt(searchParams.get('limit') || '10');
+  const skip = (page - 1) * limit;
+
+  const news = await News.find().skip(skip).limit(limit);
+
+  const totalNews = await News.countDocuments();
+
+  return NextResponse.json({
+    data: news,
+    meta: {
+      total: totalNews,
+      page,
+      limit,
+      totalPages: Math.ceil(totalNews / limit),
+    },
+  });
 }
 
 export async function POST(request) {
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,86 @@
 'use client';
-
-const logNews = async () => {
-  const res = await fetch('/api/news');
-  console.log(await res.json());
-};
+import { useEffect, useState } from 'react';
+import { createPagination } from '@/utils/createPagination';
+import { Card } from './components/Card';
 
 export default function HomePage() {
+  const [page, setPage] = useState(1);
+  const [news, setNews] = useState([]);
+  const [totalPages, setTotalPages] = useState(1);
+  const [pagination, setPagination] = useState([]);
+
+  const fetchNews = async (page) => {
+    const res = await fetch(`api/news?page=${page}&limit=9`);
+    const { data, meta } = await res.json();
+    setNews(data);
+    console.log(data);
+    setTotalPages(meta.totalPages);
+  };
+
+  useEffect(() => {
+    fetchNews(page);
+    setPagination(createPagination(totalPages, page));
+  }, [page, totalPages]);
+
+  const handlePrevious = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (page < totalPages) {
+      setPage(page + 1);
+    }
+  };
+
+  const handlePage = (item) => {
+    if (typeof item === 'number') setPage(item);
+  };
+
   return (
     <>
-      <button onClick={logNews}>Click me</button>
+      <div className="w-full">
+        <div className="w-4/6 mx-auto pt-8">
+          <h1 className="text-8xl text-center font-bold mb-6">Space News 🚀</h1>
+
+          <div className="w-full grid md:grid-cols-3 grid-cols-2 gap-x-4 gap-y-7">
+            {news.map((article) => {
+              const { _id, title, urlToImage, publishedAt, url } = article;
+              return (
+                <Card
+                  title={title}
+                  urlToImage={urlToImage}
+                  publishedAt={publishedAt?.split('T')[0]}
+                  url={url}
+                  key={_id}
+                />
+              );
+            })}
+          </div>
+
+          <div className="w-full flex gap-4 mx-auto my-8 text-2xl justify-center">
+            <button onClick={handlePrevious} disabled={page === 1}>
+              {'<'}
+            </button>
+
+            {pagination.map((item, idx) => (
+              <button
+                className={`${item === page ? 'font-bold' : ''}`}
+                key={idx}
+                disabled={item === '...'}
+                onClick={() => handlePage(item)}
+              >
+                {item}
+              </button>
+            ))}
+
+            <button onClick={handleNext} disabled={page === totalPages}>
+              {'>'}
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
